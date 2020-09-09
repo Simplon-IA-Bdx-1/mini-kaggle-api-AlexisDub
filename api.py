187dc40e4d0650dbce86a4b6379c2f33503b266f
AlexisDub <42841846+AlexisDub@users.noreply.github.com>
2020-09-09 21:03:25 +0200

    R

--- api.py
+++ api.py
@@ -1,20 +1,19 @@
 from flask import Flask, request, redirect, url_for, flash, jsonify
-import numpy as np
-import pickle as p
 import json
 import pandas as pd
+from sklearn.metrics import  roc_auc_score
 
 app = Flask(__name__)
 
 @app.route('/api/test', methods=['POST'])
 def predict():
-    df = pd.read_csv('./test2.csv')
+    df = request.files['file']
+    df = pd.read_csv(df)
     df = df['SeriousDlqin2yrs']
-    df2 = pd.read_csv('./test2-predictions.csv')
+    df2 = pd.read_csv('test2-predictions.csv')
     df2 = df2['SeriousDlqin2yrs']
-    df['comparaison'] = np.where(df['SeriousDlqin2yrs'] == df2['SeriousDlqin2yrs'], 'True', 'False')
-
-    return jsonify(df.comparaison.value_counts())
+    auc_score = roc_auc_score(df, df2)
+    return jsonify(auc_score)
 
 if __name__ == "__main__" :
     app.run()
